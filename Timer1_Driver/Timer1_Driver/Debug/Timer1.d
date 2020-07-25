9df3b650be74861d0d9414088a6947072f542e80
Msa0123654 <khaledseif166@gmail.com>
2020-07-25 05:44:54 +0200

    Timer1 Full Generic Driver

---
+++ Timer1_Driver/Timer1_Driver/Debug/Timer1.d
@@ -0,0 +1,10 @@
+Timer1.d Timer1.o: .././Timer1.c .././Timer1.h .././ATMEGA32_Regs.h \
+ .././STD.h .././BIT_Math.h
+
+.././Timer1.h:
+
+.././ATMEGA32_Regs.h:
+
+.././STD.h:
+
+.././BIT_Math.h:
---
+++ Timer1_Driver/Timer1_Driver/main.c
@@ -0,0 +1,13 @@
+
+
+
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
